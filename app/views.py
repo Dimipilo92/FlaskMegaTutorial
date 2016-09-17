45dca340e1f915b27c1ef92b09c511c70547a5f0
Dimitri <pilodi12@gmail.com>
2016-09-16 19:15:54 -0700

    Part 2.1: Templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
 # this is one of many views or handlers
-# this view handles what to do with the index URL, in this case just returning "Hello World"
+# this handler will set the index of the site to display the html
 
-from app import app 		# from this project import the flask object
+from app import app
 
-@app.route('/') 				# this will create mappings to '/' and '/index'
+@app.route('/')
 @app.route('/index')
-def index():					# this handler is called index, because it handles indexes
-	return "Hello, World" 	# essentially '/' and '/index' will display the same content
-	
+def index():
+	user = {'nickname': 'Miguel'} # fake (or mock) user object
+	return ''' 
+
+<html>
+	<head>
+		<title>Home Page</title>
+	</head>
+	<body>
+		<h1>Hello, ''' + user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
+
+# HTML is returned to the client. ''' is actually used as a longform string.
+# it is just like "" except you need to use something to nest quotes in
